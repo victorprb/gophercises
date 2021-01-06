5f023dcd622b4ed7d35239d3bf78bc9f0a43f288
victorprb <victorp.ribeiro@gmail.com>
2021-01-06 20:02:25 -0300

    Add shuffle option

--- quiz/main.go
+++ quiz/main.go
@@ -5,13 +5,16 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
+	"strings"
 	"time"
 )
 
 func main() {
 	csvFile := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
 	timeLimit := flag.Int("limit", 30, "the time limit for the quiz in seconds")
+	shuffleFlag := flag.Bool("shuffle", false, "shuffle the quiz questions order")
 	flag.Parse()
 
 	file, err := os.Open(*csvFile)
@@ -25,20 +28,22 @@ func main() {
 		log.Fatal(err)
 	}
 
+	problems := parseLines(lines)
 	score := 0
-	total := len(lines)
+	if *shuffleFlag {
+		shuffle(problems)
+	}
 	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
 
 problemloop:
-	for index, line := range lines {
-		p := problem{question: line[0], answer: line[1]}
-		fmt.Printf("Problem #%d: %s = ", index+1, p.question)
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = ", i+1, p.question)
 
 		answerCh := make(chan string)
 		go func() {
 			var answer string
 			fmt.Scanf("%s\n", &answer)
-			answerCh <- answer
+			answerCh <- strings.TrimSpace(answer)
 		}()
 
 		select {
@@ -46,16 +51,35 @@ problemloop:
 			fmt.Println()
 			break problemloop
 		case answer := <-answerCh:
-			if answer == p.answer {
+			if strings.EqualFold(answer, p.answer) {
 				score++
 			}
 		}
 	}
 
-	fmt.Printf("You scored %d of %d.\n", score, total)
+	fmt.Printf("You scored %d of %d.\n", score, len(problems))
 }
 
 type problem struct {
 	question string
 	answer   string
+}
+
+func parseLines(lines [][]string) []problem {
+	problems := make([]problem, len(lines))
+	for i, line := range lines {
+		problems[i] = problem{
+			question: line[0],
+			answer:   strings.TrimSpace(line[1]),
+		}
+	}
+
+	return problems
+}
+
+func shuffle(slice []problem) {
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(slice), func(i, j int) {
+		slice[i], slice[j] = slice[j], slice[i]
+	})
 }
