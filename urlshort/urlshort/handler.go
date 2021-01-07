9d7e2d7f0d793147990cebea9112fc5879d7a380
victorprb <victorp.ribeiro@gmail.com>
2021-01-07 08:59:55 -0300

    Add url shortener program

---
+++ urlshort/urlshort/handler.go
@@ -0,0 +1,76 @@
+package urlshort
+
+import (
+	"fmt"
+	"net/http"
+
+	"gopkg.in/yaml.v2"
+)
+
+// MapHandler will return an http.HandlerFunc (which also
+// implements http.Handler) that will attempt to map any
+// paths (keys in the map) to their corresponding URL (values
+// that each key in the map points to, in string format).
+// If the path is not provided in the map, then the fallback
+// http.Handler will be called instead.
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		if dest, ok := pathsToUrls[path]; ok {
+			http.Redirect(w, r, dest, http.StatusFound)
+			return
+		}
+		fallback.ServeHTTP(w, r)
+	}
+}
+
+// YAMLHandler will parse the provided YAML and then return
+// an http.HandlerFunc (which also implements http.Handler)
+// that will attempt to map any paths to their corresponding
+// URL. If the path is not provided in the YAML, then the
+// fallback http.Handler will be called instead.
+//
+// YAML is expected to be in the format:
+//
+//     - path: /some-path
+//       url: https://www.some-url.com/demo
+//
+// The only errors that can be returned all related to having
+// invalid YAML data.
+//
+// See MapHandler to create a similar http.HandlerFunc via
+// a mapping of paths to urls.
+func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	parsedYaml, err := parseYAML(yml)
+	if err != nil {
+		return nil, err
+	}
+	pathMap := buildMap(parsedYaml)
+	return MapHandler(pathMap, fallback), nil
+}
+
+type URLmap struct {
+	Path string
+	URL  string
+}
+
+func parseYAML(yml []byte) ([]URLmap, error) {
+	URLmaps := []URLmap{}
+
+	err := yaml.Unmarshal(yml, &URLmaps)
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		return nil, err
+	}
+
+	return URLmaps, nil
+}
+
+func buildMap(URLMaps []URLmap) map[string]string {
+	pathMap := make(map[string]string, len(URLMaps))
+	for _, v := range URLMaps {
+		pathMap[v.Path] = v.URL
+	}
+
+	return pathMap
+}
