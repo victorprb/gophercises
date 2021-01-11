23011b333f9674e7e9a5d58ad089f9627dabbe07
victorprb <victorp.ribeiro@gmail.com>
2021-01-10 21:31:31 -0300

    Refactor cyoa webapp

---
+++ cyoa/story.go
@@ -0,0 +1,14 @@
+package cyoa
+
+type Story map[string]Chapter
+
+type Chapter struct {
+	Title      string   `json:"title,omitempty"`
+	Paragraphs []string `json:"story,omitempty"`
+	Options    []Option `json:"options,omitempty"`
+}
+
+type Option struct {
+	Text    string `json:"text,omitempty"`
+	Chapter string `json:"arc,omitempty"`
+}
